878eada5989e13c33300a554f765a3a27d5966ba
KieranWardle <kieran.wardle@ons.gov.uk>
2017-10-11 15:12:20 +0100

    CTPA-1630 Saving vault config as an example

--- src/main/java/uk/gov/ons/ctp/response/sample/config/AppConfig.java
+++ src/main/java/uk/gov/ons/ctp/response/sample/config/AppConfig.java
@@ -1,11 +1,19 @@
 package uk.gov.ons.ctp.response.sample.config;
 
 import net.sourceforge.cobertura.CoverageIgnore;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import lombok.Data;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.core.env.MutablePropertySources;
 import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.vault.annotation.VaultPropertySource;
+import org.springframework.vault.core.VaultTemplate;
+
 
 /**
  * The apps main holder for centralized config read from application.yml or env
@@ -17,10 +25,21 @@ import org.springframework.retry.annotation.EnableRetry;
 @ConfigurationProperties
 @Data
 @EnableRetry
+@VaultPropertySource("secret/SampleService")
 public class AppConfig {
+
+  @Autowired
+  private VaultTemplate vaultTemplate;
+
+  @Bean
+  public void setUpVault() {
+    ConfigurableApplicationContext ctx = new GenericApplicationContext();
+    MutablePropertySources sources = ctx.getEnvironment().getPropertySources();
+  }
 
   private PartySvc partySvc;
   private SampleUnitDistribution sampleUnitDistribution;
   private DataGrid dataGrid;
   private SwaggerSettings swaggerSettings;
+
 }
